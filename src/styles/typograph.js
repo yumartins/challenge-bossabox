c38884aeb45eb7f1a3266093d3ff7c6752aacfec
yumartins <yuri@estudioflow.com.br>
2020-08-04 19:15:22 -0300

    style: typograph

---
+++ src/styles/typograph.js
@@ -0,0 +1,25 @@
+/**
+ * Typograph
+ */
+const typography = {
+  family: '"Source Sans Pro", sans-serif',
+  family_mono: '"PT Mono", sans-serif',
+
+  weight: {
+    regular: 400,
+    semiBold: 600,
+  },
+
+  size: {
+    s1: '16px',
+    s2: '18px',
+    s3: '20px',
+    m1: '24px',
+    m2: '26px',
+    l1: '30px',
+    l2: '36px',
+    l3: '42px',
+  },
+};
+
+export default typography;
