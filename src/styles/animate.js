0fdc02eff2c440e55dd1b2179383844e436a91bb
yumartins <yuri@estudioflow.com.br>
2020-08-04 19:17:54 -0300

    style: animate and module exports

---
+++ src/styles/animate.js
@@ -0,0 +1,12 @@
+/**
+ * Handy CSS animations
+ * for micro-interactions
+ */
+
+const animate = {
+  basic: 'ease-in-out',
+  rubber: 'cubic-bezier(0.175, 0.885, 0.32, 1.275)',
+  elastic: 'cubic-bezier(0.68, -0.55, 0.265, 1.55)',
+};
+
+export default animate;
---
+++ src/styles/index.js
@@ -0,0 +1,15 @@
+export {
+  default as colors,
+} from './colors';
+
+export {
+  default as animate,
+} from './animate';
+
+export {
+  default as typograph,
+} from './typograph';
+
+export {
+  default as GlobalStyles,
+} from './global';
