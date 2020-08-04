d065570edb9d93895bb87f28f6a32debdec64012
yumartins <yuri@estudioflow.com.br>
2020-08-04 19:10:58 -0300

    style: colors

---
+++ src/styles/colors.js
@@ -0,0 +1,75 @@
+/**
+ * Colors
+ */
+const colors = {
+  ink: {
+    base: '#170C3A',
+    light: '#8F8A9B',
+    lighter: '#B1ADB9',
+    lightest: '#C7C4CD',
+  },
+
+  red: {
+    darker: '#A53F3F',
+    dark: '#CC4C4C',
+    base: '#F95E5A',
+    light: '#FCAEAC',
+    lighter: '#FCC6C5',
+    lightest: '#FCD7D6',
+    most_lightest: '#FEEFEE',
+  },
+
+  green: {
+    darker: '#0E995D',
+    dark: '#10B26C',
+    base: '#12DB89',
+    light: '#88EDC4',
+    lighter: '#B7F7D8',
+    lightest: '#CFF9E6',
+    most_lightest: '#E7FBF3',
+  },
+
+  blue: {
+    darker: '#244AA8',
+    dark: '#2F55CC',
+    base: '#365DF0',
+    light: '#9AAEF7',
+    lighter: '#B9C6FA',
+    lightest: '#CAD6FC',
+    most_lightest: '#E1E7FD',
+  },
+
+  teal: {
+    darker: '#2497B2',
+    dark: '#2BAFD8',
+    base: '#30C3FC',
+    light: '#98E1FE',
+    lighter: '#EAF9FF',
+  },
+
+  purple: {
+    darker: '#353372',
+    dark: '#4C4499',
+    base: '#6554C0',
+    light: '#B2A9DF',
+    lighter: '#EFEEF9',
+  },
+
+  yellow: {
+    darker: '#996C2B',
+    dark: '#D89C3A',
+    base: '#FFBB43',
+    light: '#FFDDA1',
+    lighter: '#FFF8EC',
+  },
+
+  white: {
+    base: '#FFFFFF',
+    dark: '#FCFCFD',
+    darker: '#F5F4F6',
+    darkest: '#EBEAED',
+    most_darkest: '#DEDCE1',
+  },
+};
+
+export default colors;
