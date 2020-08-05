30d2abf377e29511e073feb004884af00e3f5c27
yumartins <yuri@estudioflow.com.br>
2020-08-05 17:39:13 -0300

    refactor: json server api

--- packages/challenge-api/src/index.js
+++ packages/challenge-api/src/index.js
@@ -1,12 +1,8 @@
 const cors = require('cors');
 const express = require('express');
 
-const database = require('./backend/db.json');
-
 const app = express();
 
 app.use(cors());
-
-app.all('/', (req, res) => res.json(database));
 
 app.listen(5000, () => console.log('Ready api => 5000'));
